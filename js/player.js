cbeedc1b2c2eb6000c9b95f8bb3f224c41ac4fbf
HannesNyfelt <hannes.nyfelt@gmail.com>
2022-02-09 10:38:32 +0200

    input

---
+++ js/player.js
@@ -0,0 +1,22 @@
+import { getKey, keyCodes } from "./input.js";
+import { calcPhysics } from "./phys.js";
+
+
+export function updatePlayer(player) {
+
+    calcPhysics(player);
+
+    if (player.grounded && getKey(keyCodes.arrowUp)) {
+        player.Vy = -10;
+        player.grounded = false;
+    }
+
+    player.Vx += getKey(keyCodes.arrowLeft) ? -5 : 0;
+    player.Vx += getKey(keyCodes.arrowRight) ? 5 : 0;
+
+    if (player.y > 450 - player.height) {
+        player.y = 450 - player.height;
+        player.Vy = 0;
+        player.grounded = true;
+    }
+}
